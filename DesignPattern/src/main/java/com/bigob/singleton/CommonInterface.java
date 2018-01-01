217a83fc6979b4d97364e7e28fa18e27a09617a6
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-01 19:21:50 +0530

    Clonable and Serialization added

---
+++ DesignPattern/src/main/java/com/bigob/singleton/CommonInterface.java
@@ -0,0 +1,12 @@
+package com.bigob.singleton;
+
+import java.io.Serializable;
+
+public class CommonInterface implements Serializable,Cloneable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- DesignPattern/src/main/java/com/bigob/singleton/SingletonClass.java
+++ DesignPattern/src/main/java/com/bigob/singleton/SingletonClass.java
@@ -3,8 +3,13 @@ package com.bigob.singleton;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class SingletonClass {
+public class SingletonClass extends CommonInterface {
 
+	/**
+	 * This is because if we send the data through serialization then it may be or may not be the the 
+	 * same system or same OS so it is good to generate serialVersionUID
+	 */
+	private static final long serialVersionUID = -3488209126190051707L;
 	private static SingletonClass singletonClass = null;
 
 	private SingletonClass() {
